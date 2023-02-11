59a30845db51e75f1beb319fbf68d2b60afbbdd0
LazyCow <123736479+nft-dna@users.noreply.github.com>
2023-02-11 10:14:15 +0100

    Update MutantCawSeed.sol
    - code review

--- MutantCawSeed.sol
+++ MutantCawSeed.sol
@@ -15,19 +15,19 @@ contract MutantCawSeed is ERC20, Ownable {
 	//  Apr 30 2033 13:33:33 GMT+0000
 	uint256 constant public END = 1998480813;
 
-	mapping(address => uint256) public rewards;
-	mapping(address => uint256) public lastUpdate;
+	mapping(address => uint256) public Rewards;
+	mapping(address => uint256) public LastUpdate;
 
-	IMBBcontract public  _MBBContract;
+	IMBBcontract public  MBBContract;
 
 	event RewardPaid(address indexed user, uint256 reward);
 
     constructor(address mbbcntr) ERC20("MutantCawSeed", "MCS") {
-		_MBBContract = IMBBcontract(mbbcntr);
+		MBBContract = IMBBcontract(mbbcntr);
 	}
 
     function mint(address to, uint256 amount) public {
-    	require (msg.sender == address(_MBBContract) || msg.sender == owner());
+    	require (msg.sender == address(MBBContract) || msg.sender == owner());
         _mint(to, amount);
     }
 
@@ -38,52 +38,52 @@ contract MutantCawSeed is ERC20, Ownable {
     // called when minting many NFTs
 	// updated_amount = (balanceOG(user) * base_rate * delta / 86400) + amount * initial rate
 	function updateRewardOnMint(address _user, uint16 _amount) external {
-		require(msg.sender == address(_MBBContract), "Can't call this");
+		require(msg.sender == address(MBBContract), "Can't call this");
 		uint256 time = min(block.timestamp, END);
-		uint256 timerUser = lastUpdate[_user];
+		uint256 timerUser = LastUpdate[_user];
         if (timerUser > 0) {
-            uint256 reward = rewards[_user] + (_MBBContract.balanceOf(_user) * BASE_RATE_XSEC * (time - timerUser));
+            uint256 reward = Rewards[_user] + (MBBContract.balanceOf(_user) * BASE_RATE_XSEC * (time - timerUser));
             reward = reward + _amount * MINT_GIFT;
-			rewards[_user] = reward;
+			Rewards[_user] = reward;
         }
 		else {
-			rewards[_user] = rewards[_user] + _amount * MINT_GIFT;
+			Rewards[_user] = Rewards[_user] + _amount * MINT_GIFT;
         }
-		lastUpdate[_user] = time;
+		LastUpdate[_user] = time;
 	}
 
 	// called on transfers
 	function updateReward(address _from, address _to/*, uint256 _tokenId*/) external {
-		require(msg.sender == address(_MBBContract));
+		require(msg.sender == address(MBBContract));
 		//if (_tokenId < 1001) {
 			uint256 time = min(block.timestamp, END);
-			uint256 timerFrom = lastUpdate[_from];
+			uint256 timerFrom = LastUpdate[_from];
 			if (timerFrom > 0)
-				rewards[_from] += (_MBBContract.balanceOf(_from) * BASE_RATE_XSEC * (time - timerFrom));
-			if (lastUpdate[_from] != END)
-				lastUpdate[_from] = time;
+				Rewards[_from] += (MBBContract.balanceOf(_from) * BASE_RATE_XSEC * (time - timerFrom));
+			if (LastUpdate[_from] != END)
+				LastUpdate[_from] = time;
 			if (_to != address(0)) {
-				uint256 timerTo = lastUpdate[_to];
+				uint256 timerTo = LastUpdate[_to];
 				if (timerTo > 0)
-					rewards[_to] += (_MBBContract.balanceOf(_to) * BASE_RATE_XSEC * (time - timerTo));
-				if (lastUpdate[_to] != END)
-					lastUpdate[_to] = time;
+					Rewards[_to] += (MBBContract.balanceOf(_to) * BASE_RATE_XSEC * (time - timerTo));
+				if (LastUpdate[_to] != END)
+					LastUpdate[_to] = time;
 			}
 		//}
 	}
 
 	function getReward(address _to) external {
-		require(msg.sender == address(_MBBContract));
-		uint256 reward = rewards[_to];
+		require(msg.sender == address(MBBContract));
+		uint256 reward = Rewards[_to];
 		if (reward > 0) {
-			rewards[_to] = 0;
+			Rewards[_to] = 0;
 			_mint(_to, reward);
 			emit RewardPaid(_to, reward);
 		}
 	}
 
 	function burn(address _from, uint256 _amount) external {
-		require(msg.sender == address(_MBBContract));
+		require(msg.sender == address(MBBContract));
 		_burn(_from, _amount);
 	}
 	
@@ -94,8 +94,8 @@ contract MutantCawSeed is ERC20, Ownable {
 	function getTotalClaimable(address _user) external view returns(uint256) {
 		uint256 time = min(block.timestamp, END);
         uint256 pending = 0;
-        if (lastUpdate[_user] > 0 && time > lastUpdate[_user])
-            pending = (_MBBContract.balanceOf(_user) * BASE_RATE_XSEC * (time- lastUpdate[_user]));
-		return rewards[_user] + pending;
+        if (LastUpdate[_user] > 0 && time > LastUpdate[_user])
+            pending = (MBBContract.balanceOf(_user) * BASE_RATE_XSEC * (time - LastUpdate[_user]));
+		return Rewards[_user] + pending;
 	}
 }
