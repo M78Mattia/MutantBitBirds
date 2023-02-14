1bee4bc77cda25c10fceb7955397036693a8a3ee
LazyCow <123736479+nft-dna@users.noreply.github.com>
2023-02-14 11:49:22 +0100

    Create Interfaces.sol
    - code refactoring (interfaces and separate tokenUri logic)

---
+++ Interfaces.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+import "@openzeppelin/contracts/utils/Strings.sol";
+
+interface IMainContract {
+    function getNickName(uint256 tokenId) external view returns (string memory );    
+	function balanceOf(address account) external view returns(uint256);
+    function getRewardContract() external view returns(address); 
+}
+
+interface ITraitChangeCost {
+    struct TraitChangeCost {
+        bool allowed;
+        uint32 changeCostEthMillis;
+        uint32 increaseStepCostEthMillis;
+        uint32 decreaseStepCostEthMillis;
+        uint8 minValue;
+        uint8 maxValue;
+    }
+}
