7315edf7f4eff8371d2eeb66f9d16e3dd417bd38
LazyCow <123736479+nft-dna@users.noreply.github.com>
2023-02-14 11:53:35 +0100

    Update and rename MutantCawSeed.sol to YieldTokenContract.sol
    - code refactoring (interfaces and separate tokenUri logic)

--- YieldTokenContract.sol
+++ YieldTokenContract.sol
@@ -3,12 +3,10 @@ pragma solidity ^0.8.17;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "./Interfaces.sol";
 
-interface IMBBcontract {
-	function balanceOf(address _user) external view returns(uint256);
-}
 
-contract MutantCawSeed is ERC20, Ownable {
+contract YieldTokenContract is ERC20, Ownable {
 
 	uint256 constant public BASE_RATE_XSEC = 34722222222223; // * 3 eth (daily) / 86400 (seconds in a day)
 	uint256 constant public MINT_GIFT = 100 ether;
@@ -18,16 +16,16 @@ contract MutantCawSeed is ERC20, Ownable {
 	mapping(address => uint256) public Rewards;
 	mapping(address => uint256) public LastUpdate;
 
-	IMBBcontract public  MBBContract;
+	IMainContract public MainContract;
 
 	event RewardPaid(address indexed user, uint256 reward);
 
     constructor(address mbbcntr) ERC20("MutantCawSeed", "MCS") {
-		MBBContract = IMBBcontract(mbbcntr);
+		MainContract = IMainContract(mbbcntr);
 	}
 
     function mint(address to, uint256 amount) public {
-    	require (msg.sender == address(MBBContract) || msg.sender == owner());
+    	require (msg.sender == address(MainContract) || msg.sender == owner());
         _mint(to, amount);
     }
 
@@ -38,11 +36,11 @@ contract MutantCawSeed is ERC20, Ownable {
     // called when minting many NFTs
 	// updated_amount = (balanceOG(user) * base_rate * delta / 86400) + amount * initial rate
 	function updateRewardOnMint(address user, uint16 amount) external {
-		require(msg.sender == address(MBBContract), "Can't call this");
+		require(msg.sender == address(MainContract), "Can't call this");
 		uint256 time = min(block.timestamp, END);
 		uint256 timerUser = LastUpdate[user];
         if (timerUser > 0) {
-            uint256 reward = Rewards[user] + (MBBContract.balanceOf(user) * BASE_RATE_XSEC * (time - timerUser));
+            uint256 reward = Rewards[user] + (MainContract.balanceOf(user) * BASE_RATE_XSEC * (time - timerUser));
             reward = reward + amount * MINT_GIFT;
 			Rewards[user] = reward;
         }
@@ -54,18 +52,18 @@ contract MutantCawSeed is ERC20, Ownable {
 
 	// called on transfers
 	function updateReward(address from, address to/*, uint256 _tokenId*/) external {
-		require(msg.sender == address(MBBContract));
+		require(msg.sender == address(MainContract));
 		//if (_tokenId < 1001) {
 			uint256 time = min(block.timestamp, END);
 			uint256 timerFrom = LastUpdate[from];
 			if (timerFrom > 0 && time > timerFrom)
-				Rewards[from] = Rewards[from] + (MBBContract.balanceOf(from) * BASE_RATE_XSEC * (time - timerFrom));
+				Rewards[from] = Rewards[from] + (MainContract.balanceOf(from) * BASE_RATE_XSEC * (time - timerFrom));
 			if (timerFrom != END && time > timerFrom)
 				LastUpdate[from] = time;
 			if (to != address(0)) {
 				uint256 timerTo = LastUpdate[to];
 				if (timerTo > 0 && time > timerTo)
-					Rewards[to] = Rewards[to] + (MBBContract.balanceOf(to) * BASE_RATE_XSEC * (time - timerTo));
+					Rewards[to] = Rewards[to] + (MainContract.balanceOf(to) * BASE_RATE_XSEC * (time - timerTo));
 				if (timerTo != END && time > timerTo)
 					LastUpdate[to] = time;
 			}
@@ -73,7 +71,7 @@ contract MutantCawSeed is ERC20, Ownable {
 	}
 
 	function getReward(address to) external {
-		require(msg.sender == address(MBBContract));
+		require(msg.sender == address(MainContract));
 		uint256 reward = Rewards[to];
 		if (reward > 0) {
 			Rewards[to] = 0;
@@ -83,12 +81,12 @@ contract MutantCawSeed is ERC20, Ownable {
 	}
 
 	function burn(address from, uint256 amount) external {
-		require(msg.sender == address(MBBContract));
+		require(msg.sender == address(MainContract));
 		_burn(from, amount);
 	}
 	
 	function collect(address from, uint256 amount) external {
-		require(msg.sender == address(MBBContract));
+		require(msg.sender == address(MainContract));
 		uint256 rew = Rewards[from];
 		require(rew >= amount, "amount");
 		Rewards[from] = rew - amount;
@@ -99,7 +97,7 @@ contract MutantCawSeed is ERC20, Ownable {
 		uint256 time = min(block.timestamp, END);
         uint256 pending = 0;
         if (LastUpdate[user] > 0 && time > LastUpdate[user])
-            pending = (MBBContract.balanceOf(user) * BASE_RATE_XSEC * (time - LastUpdate[user]));
+            pending = (MainContract.balanceOf(user) * BASE_RATE_XSEC * (time - LastUpdate[user]));
 		return Rewards[user] + pending;
 	}
 }
