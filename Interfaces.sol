61ed0e705a48a7e9ac2ff26c97f78abf2e98fca2
LazyCow <123736479+nft-dna@users.noreply.github.com>
2023-02-24 19:25:08 +0100

    Update Interfaces.sol
    - formatted code

--- Interfaces.sol
+++ Interfaces.sol
@@ -4,15 +4,18 @@ pragma solidity ^0.8.17;
 import "@openzeppelin/contracts/utils/Strings.sol";
 
 interface IMainContract {
-	function getNickName(uint256 tokenId) external view returns (string memory );    
-	function balanceOf(address account) external view returns(uint256);
-	function getRewardContract() external view returns(address); 
+    
+    function getNickName(uint256 tokenId) external view returns (string memory);
+
+    function balanceOf(address account) external view returns (uint256);
+
+    function getRewardContract() external view returns (address);
 }
 
 interface ITraitChangeCost {
     struct TraitChangeCost {
         uint8 minValue;
-        uint8 maxValue;    
+        uint8 maxValue;
         bool allowed;
         uint32 changeCostEthMillis;
         uint32 increaseStepCostEthMillis;
