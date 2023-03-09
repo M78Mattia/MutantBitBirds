ab58f85a3c0cb379b5c70a47ce176f63ee27e7eb
LazyCow <123736479+nft-dna@users.noreply.github.com>
2023-03-09 19:47:01 +0100

    Update MainContract.sol
    - code review

--- MainContract.sol
+++ MainContract.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.17;
 
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/IERC721Metadata.sol";
+//import "@openzeppelin/contracts/token/ERC721/extensions/IERC721Metadata.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
 import "@openzeppelin/contracts/security/Pausable.sol";
